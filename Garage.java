767e45955d24b30c655e0fea44e3f823a2fd4880
YazanRub <yazanalirub2006@gmail.com>
2026-04-01 08:33:36 +0300

    Implement vehicle management methods in Garage class
    Added methods for adding, removing, searching, and maintaining vehicles in the Garage class.

--- Garage.java
+++ Garage.java
@@ -1,3 +1,47 @@
 public class Garage {
-    
+     private GarageEquipments equipments;
+    private Vehicle vehicle[];
+    private int numOfVehicles;
+    private int maxCapacity;
+
+    public Garage (int maxCapacity){
+        this.maxCapacity=maxCapacity;
+        vehicle = new Vehicle[maxCapacity];
+    }
+    public boolean addVehicle(Vehicle v){
+        if(numOfVehicles<vehicle.length){
+            numOfVehicles++;
+            return true;
+        }return false;
+
+    }
+    public  boolean removeVehicle(int plateNum){
+        if(plateNum==getPlateNum){
+            numOfVehicles--;
+            return true;
+        } return false;
+
+    }
+    public Vehicle searchVehicle(int plateNum){
+        for(int i =0; i<=maxCapacity;i++){
+            if(vehicle[i].getPlateNum==plateNum)
+                return vehicle[i];
+        } return null;
+
+
+    }
+    public void maintainVehicle(int plateNum){
+        if(searchVehicle(plateNum)!=null){
+            if(Maintainable.)
+        }
+
+
+
+        }
+
+    public void displayAllVehicle(){
+        for(int i = 0; numOfVehicles>=maxCapacity;i++){
+            vehicle[i].printInfo();
+        }
+    }
 }
