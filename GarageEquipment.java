7880174b0bd75ac5280e29c6a20c1d3a0735ac2f
YazanRub <yazanalirub2006@gmail.com>
2026-04-01 08:32:10 +0300

    Implement GarageEquipment class with health management

--- GarageEquipment.java
+++ GarageEquipment.java
@@ -1,3 +1,21 @@
 public class GarageEquipment implements Maintainable {
+    private int health;
+
+    public GarageEquipments (int health){
+        this.health= health;
+    }
+
+    public void use(){
+        health=-10;
+    }
+
+    public void performMaintenance(){
+        health=100;
+    }
+
+    public int getHealth(){
+        return health;
+    }
+
     
 }
