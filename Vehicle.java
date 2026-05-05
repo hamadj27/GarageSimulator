2c11519f55ca19cea52d7b5cbbdb16ea37d070f4
hamadj27 <hamadjar26@gmail.com>
2026-05-05 11:21:20 +0300

    exception handling fixes

--- Vehicle.java
+++ Vehicle.java
@@ -8,11 +8,13 @@ public abstract class Vehicle implements Maintainable, Serializable {
 
     // initializes the common properties of any vehicle 
     public Vehicle(String brand, String color, double engineSize, int cylinders, int HP, int plateNum) {
-        this.brand = brand;
-        this.color = color;
-        engine = new Engine(engineSize, cylinders, HP);
-        this.plateNum = plateNum;
+        LinkedList<String> errors = new LinkedList<>();
+        setBrand(brand, errors);
+        setColor(color, errors);
+        engine = new Engine(engineSize, cylinders, HP, errors);
+        setPlateNum(plateNum, errors);
         isReady = false;
+        if (!errors.isEmpty()) throw new VehicleExceptions(errors);
     }
 
     //abstract method to print vehicle details, implemented by child classes
@@ -24,13 +26,39 @@ public abstract class Vehicle implements Maintainable, Serializable {
     //getters:
 
     public String getBrand() {return brand;}
-    public void setBrand(String brand) {this.brand = brand;}
+    private void setBrand(String brand, LinkedList<String> errors) {
+        for(int i = 0; i < brand.length(); i++) {
+            if(Character.isDigit(brand.charAt(i))) {
+                errors.addLast("Brand name cannot contain numbers.");
+                return;
+            }
+        }
+
+        this.brand = brand;
+    }
 
     public String getColor() {return color;}
+    private void setColor(String color, LinkedList<String> errors) {
+        for(int i = 0; i < color.length(); i++)  {
+            if(Character.isDigit(color.charAt(i))) {
+                errors.addLast("Color cannot contain numbers."); 
+                return;
+            }
+        }
+        
+        this.color = color;
+}
 
     public Engine getEngine() {return engine;}
 
     public int getPlateNum() {return plateNum;}
+    private void setPlateNum(int p, LinkedList<String> errors) {
+        if (p < 0) errors.addLast("Plate number cannot be negative.");
+        else if (p == 0) errors.addLast("Plate number cannot be ZERO.");
+        else
+        this.plateNum = p;
+    }
+
 
     public boolean isReady() {return isReady;}
 }
